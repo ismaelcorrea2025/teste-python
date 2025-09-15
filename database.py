5356460c54b1ffccccd61bb9367cfc88980020e5
mael <eletroomaster@gmail.com>
2025-09-15 15:44:29 -0300

    versao final 1.1

--- database.py
+++ database.py
@@ -7,3 +7,12 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+from sqlalchemy.orm import Session
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
